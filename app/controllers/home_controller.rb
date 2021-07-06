117f969026c55ae4dcf36d82f8f2c46b2eb4f40e
Ivan Danyliv <i.danyliv@bvblogic.com>
2021-07-06 14:49:51 +0300

    Finished task

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+
   def index;
     @projects = Project.find_status(params[:f_status]).last(5)
   end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,9 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show]
+  before_action :more_projexcts, only: %i[index]
 
-  def index
-    @projects = Project.find_status(params[:f_status])
+  def index    
+    @projects = Project.find_status(params[:f_status]).last(session[:count_projects])
   end
 
   private
@@ -17,6 +18,14 @@ class ProjectsController < ApplicationController
     #                                 :name, :name_en, :slug, :title, :title_en,
     #                                 :short_description, :short_description_en, :description,
     #                                 :description_en, :site, :url_facebook, :need_amount)
-    params.require(:project).permit(:f_status)
+    params.require(:project).permit(:f_status, :count_projects)
+  end
+  
+  def more_projexcts
+    if params[:count_projects].blank?
+      session[:count_projects] = 5
+    else
+      session[:count_projects] += params[:count_projects].to_i
+    end
   end
 end
