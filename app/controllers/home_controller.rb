2bad962c388e9682197dfe607155539fab3990aa
Ivan Danyliv <i.danyliv@bvblogic.com>
2021-07-08 13:44:57 +0300

    Fixed bugs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :more_projexcts, only: %i[index]
 
-  def index;
-    @projects = Project.find_status(params[:f_status]).last(5)
+  def index
+    @projects = Project.find_status(params[:f_status]).order(id: :asc).first(session[:count_projects])
+  end
+
+  private
+
+  def more_projexcts
+    if params[:count_projects].blank?
+      session[:count_projects] = 5
+    else
+      session[:count_projects] = session[:count_projects].to_i + params[:count_projects].to_i
+    end
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   before_action :more_projexcts, only: %i[index]
 
   def index    
-    @projects = Project.find_status(params[:f_status]).last(session[:count_projects])
+    @projects = Project.find_status(params[:f_status]).order(id: :asc).first(session[:count_projects])
   end
 
   private
@@ -25,7 +25,7 @@ class ProjectsController < ApplicationController
     if params[:count_projects].blank?
       session[:count_projects] = 5
     else
-      session[:count_projects] += params[:count_projects].to_i
+      session[:count_projects] = session[:count_projects].to_i + params[:count_projects].to_i
     end
   end
 end
